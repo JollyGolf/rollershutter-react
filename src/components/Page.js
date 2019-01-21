45d2f11e6542c15ad73c1b571406e9ffa49502f2
jollygolf <zhigamovky>
2019-01-21 19:22:13 +0200

    Empty project

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Page extends Component {
+  render() {
+    return (
+      <div className="Page">
+        React App
+      </div>
+    );
+  }
+}
+
+export default Page;
